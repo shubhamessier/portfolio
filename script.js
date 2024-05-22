de085b72007a5d6f1e4f370b8bd4624599d4c783
shubhamessier <shubham.gaur7116@gmail.com>
2024-05-22 13:29:02 +0530

    minor fixes

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById("theme-toggle").addEventListener("click", (e) => {
-  const checked = e.target.checked;
-  document.body.setAttribute("theme", checked ? "dark" : "light");
+const paragraphs = document.querySelectorAll(".section_paragraph");
+
+document.addEventListener("scroll", function () {
+  paragraphs.forEach((paragraph) => {
+    if (isInView(paragraph)) {
+      paragraph.classList.add("section_paragraph--visible");
+    }
+  });
 });
+
+function isInView(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
